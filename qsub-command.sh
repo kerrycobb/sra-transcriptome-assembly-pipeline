6df1ebe918c688ac7026713f9d1135db4b15b1e9
kerrycobb <cobbkerry@gmail.com>
2017-12-19 22:35:50 -0600

    major changes

---
+++ qsub-command.sh
@@ -0,0 +1,16 @@
+snakemake \
+--configfile $1
+--workdir $2
+--cores 20 \
+--latency 60 \
+--rerun-incomplete \
+--cluster \
+"ssh kac0070@hopper.auburn.edu \
+'/cm/shared/apps/torque/5.1.0/bin/qsub \
+-l nodes=1:ppn={threads},mem={params.mem},walltime={params.time} \
+-N {rule}.{wildcards.sample} \
+-m n \
+-j oe \
+-d {workingdir} \
+-o {log} \
+' "
---
+++ run-pipeline.py
@@ -0,0 +1,37 @@
+#!/usr/bin/env python
+
+import click
+import os
+import os.path as p
+import yaml
+
+@click.command()
+@click.argument('configfile')
+
+def cli(configfile):
+    configfile = p.abspath(configfile)
+    name = p.splitext(p.basename(configfile))[0]
+    workdir = p.join(os.getcwd(), name)
+    if not p.exists(workdir):
+        os.makedirs(workdir)
+
+    proc =(
+        'qsub '
+        '-l nodes=1:ppn=1,mem=250mb,walltime=168:00:00 '
+        '-N {name} '
+        '-j oe '
+        '-m e '
+        '-M cobbkerry@gmail.com '
+        '-d {workdir} '
+        '-o logs/ '
+        'qsub-command.sh {configfile} {workdir}'
+    ).format(
+        name=name,
+        workdir=workdir,
+        configfile=configfile,
+        )
+
+    os.system(proc)
+
+if __name__=='__main__':
+    cli()
